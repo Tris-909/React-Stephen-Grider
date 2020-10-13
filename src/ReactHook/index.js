f1edc89da8656de31ba345cbb4b64b985f54f7e2
= <tranminhtri9090@gmail.com>
2020-10-14 09:13:54 +1000

    Finish accordion widget

--- src/ReactHook/index.js
+++ src/ReactHook/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+    {
+        title: 'What is React ?',
+        content: 'React is a front end library framework'
+    },
+    {
+        title: 'What is the date today ?',
+        content: '14/10/2020'
+    },
+    {
+        title: 'Are u tired ?',
+        content: 'Dont die'
+    }
+];
 
 const Widget = () => {
-
     return(
-        <div>Widget</div>
+        <div>
+            <Accordion items={items} />
+        </div>
     )
 }
 
